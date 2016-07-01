21e500dcdee4e3cb6ddc02e68725e78c7b71cf87
Vijay Joshi <Vijay Joshi>
2016-06-30 23:36:20 -0700

    Koans Project

---
+++ koans/src/beginner/AboutObjects.java
@@ -0,0 +1,67 @@
+package beginner;
+
+import com.sandwich.koan.Koan;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static com.sandwich.koan.constant.KoanConstants.__;
+import static com.sandwich.util.Assert.assertEquals;
+
+public class AboutObjects {
+
+    @Koan
+    public void newObjectInstancesCanBeCreatedDirectly() {
+        assertEquals(new Object() instanceof Object, true);
+    }
+
+    @Koan
+    public void allClassesInheritFromObject() {
+        class Foo {
+        }
+
+        Class<?>[] ancestors = getAncestors(new Foo());
+        assertEquals(ancestors[0], Foo.class);
+        assertEquals(ancestors[1], Object.class);
+    }
+
+    @Koan
+    public void objectToString() {
+        Object object = new Object();
+        String expectedToString = MessageFormat.format("{0}@{1}", Object.class.getName(), Integer.toHexString(object.hashCode()));
+        assertEquals(expectedToString, __); // hint: object.toString()
+    }
+
+    @Koan
+    public void toStringConcatenates() {
+        final String string = "ha";
+        Object object = new Object() {
+            @Override
+            public String toString() {
+                return string;
+            }
+        };
+        assertEquals(string + object, __);
+    }
+
+    @Koan
+    public void toStringIsTestedForNullWhenInvokedImplicitly() {
+        String string = "string";
+        Integer i = new Integer(128);
+        Short s = new Short((short) 15);
+        assertEquals(string + " " + i + " " + s, __);
+    }
+
+    private Class<?>[] getAncestors(Object object) {
+        List<Class<?>> ancestors = new ArrayList<Class<?>>();
+        Class<?> clazz = object.getClass();
+        while (clazz != null) {
+            ancestors.add(clazz);
+            clazz = clazz.getSuperclass();
+        }
+        return ancestors.toArray(new Class[]{});
+    }
+
+}

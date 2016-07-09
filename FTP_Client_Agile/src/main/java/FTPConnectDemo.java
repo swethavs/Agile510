95a347cbb049bb50cded83e8e78d9927f7417b93
Xi Pardee <pardeexi@pdx.edu>
2016-07-09 14:43:22 -0700

    Refactor (first story) and command ls (second story) is done.

--- FTP_Client_Agile/src/main/java/FTPConnectDemo.java
+++ FTP_Client_Agile/src/main/java/FTPConnectDemo.java
@@ -5,34 +5,51 @@ import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpException;
 import java.io.PrintStream;
+import java.util.Scanner;
+
 import org.apache.commons.net.ftp.FTPClient;
 
 public class FTPConnectDemo {
+
     private static final int FTP_TIMEOUT = 3000;
+    private static Scanner scanner = new Scanner(System.in).useDelimiter("\n");
 
     public static void main(String[] args) {
-        FTPClient ftpClient = new FTPClient();
-        String user = "pmudgal";
-        String host = "linuxlab.cs.pdx.edu";
-        int port = 22;
-        String pass = ""; //Pass the password here
-        try {
-            JSch jsch = new JSch();
-            Session session = jsch.getSession(user, host, port);
-            session.setConfig("StrictHostKeyChecking", "no");
-            session.setPassword(pass);
-            session.connect(3000);
-            Channel channel = session.openChannel("sftp");
-            ChannelSftp sftp = (ChannelSftp)channel;
-            sftp.connect(3000);
-            sftp.mkdir("testing");
-            System.out.println("Connected");
+
+        //String host = "linuxlab.cs.pdx.edu";
+        String host = args[0];
+
+        int port = Integer.parseInt(args[1]);
+
+        //String user = "pmudgal";
+        String user = args[2];
+
+        //String password;
+        String pass = args[3];
+
+        SessionController controller = new SessionController();
+
+        if (!controller.setUpConnection(host, port, user, pass)) {
+            System.out.println("Connection fails");
+            System.exit(1);
         }
-        catch (JSchException e) {
-            e.printStackTrace();
-        }
-        catch (SftpException e) {
-            e.printStackTrace();
+
+        userIO(controller);
+        controller.closeSession();
+
+    }
+
+    //FTPClient ftpClient = new FTPClient();
+
+    public static void userIO(SessionController controller) {
+        System.out.println("Please enter your command:");
+        String command = scanner.next();
+        while (!command.equalsIgnoreCase("q")) {
+
+            controller.execCommand(command);
+            System.out.println("Please enter your command:");
+            command = scanner.next();
+
         }
     }
 }

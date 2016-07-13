36b3ce192f5522d80bd7668b8e9f5ceeb770a96d
Priyanka Mudgal <priyankamudgal19@gmail.com>
2016-07-12 19:06:55 -0700

    Removed hardcoded connection param

--- FTP/src/main/java/FTPConnectDemo.java
+++ FTP/src/main/java/FTPConnectDemo.java
@@ -13,48 +13,6 @@ import org.apache.commons.net.ftp.FTPClient;
 
 public class FTPConnectDemo {
 
-    /*private static final int FTP_TIMEOUT = 3000;
-    private static Scanner scanner = new Scanner(System.in).useDelimiter("\n");
-*/
-  /*  public static void main(String[] args) {
-
-        //String host = "linuxlab.cs.pdx.edu";
-        String host = args[0];
-
-        int port = Integer.parseInt(args[1]);
-
-        //String user = "pmudgal";
-        String user = args[2];
-
-        //String password;
-        String pass = args[3];
-
-        SessionController controller = new SessionController();
-
-        if (!controller.setUpConnection(host, port, user, pass)) {
-            System.out.println("Connection fails");
-            System.exit(1);
-        }
-
-        userIO(controller);
-        controller.closeSession();
-
-    }
-
-    //FTPClient ftpClient = new FTPClient();
-
-    public static void userIO(SessionController controller) {
-        System.out.println("Please enter your command:");
-        String command = scanner.next();
-        while (!command.equalsIgnoreCase("q")) {
-
-            controller.execCommand(command);
-            System.out.println("Please enter your command:");
-            command = scanner.next();
-
-        }
-    }*/
-
     private static final int FTP_TIMEOUT = 3000;
     private static Scanner scanner = new Scanner(System.in).useDelimiter("\n");
     private static Properties prop = new Properties();
